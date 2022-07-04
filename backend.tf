2ccc0646ee7bc67c3e3d529e3d2ad56319e1ade7
umapallaa <106008138+umapallaa@users.noreply.github.com>
2022-07-04 12:43:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "mydev-tf-state-bucket11"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table11"
   }
 }
